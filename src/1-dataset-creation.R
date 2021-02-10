54c1d8b0d5efe5d3942cb14f59d0ce8386ff0a0a
Andrew Mertens <amertens@berkeley.edu>
2021-02-10 12:33:46 -0800

    update script organization

--- src/1-dataset-creation.R
+++ src/1-dataset-creation.R
@@ -4,7 +4,6 @@
 rm(list=ls())
 
 source(here::here("0-config.R"))
-source(here::here("src/0-gam-functions.R"))
 
 #Load stress-growth data
 d<-readRDS("data/stress_growth_data.RDS")
@@ -27,7 +26,10 @@ dev <- dev %>%
     z_combined_easq_t3 = z_combined_easq
   )
 ## Merge datasets
+dim(d)
+dim(dev)
 dfull <- left_join(d, dev, by="childid")
+dim(dfull)
 
 #Save new dataset
 saveRDS(dfull, paste0(dropboxDir,"Data/Cleaned/Audrie/stress-dev.RDS"))
--- src/4-stress-dev-adj-emm.R
+++ src/4-stress-dev-adj-emm.R
@@ -1,3 +1,4 @@
+
 rm(list=ls())
 
 source(here::here("0-config.R"))
