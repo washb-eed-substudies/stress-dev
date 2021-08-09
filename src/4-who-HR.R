e17735e779d594103eae3d737f12fda6929c9fe4
Zach <zbutzin@gmail.com>
2021-08-09 16:52:18 -0400

    HR analyses: error from fit_HR_GAM: "Error in `[.data.frame`(x, r, vars, drop = drop) :   undefined columns selected"

--- src/4-who-HR.R
+++ src/4-who-HR.R
@@ -10,18 +10,25 @@ d <- readRDS(paste0(dropboxDir, "Data/Cleaned/Audrie/stress-dev.RDS"))
 
 Xvars <- c("t2_f2_8ip", "t2_f2_23d", "t2_f2_VI", "t2_f2_12i", "t2_f2_iso.pca")    
 
-Yvars <- c("sit_nosupp", "crawl_nosupp", "stand_supp",
-           "walk_supp", "stand_nosupp", "walk_nosupp" )
+Yvars <- c("who_sit_nosupp", "who_crawl_nosupp", "who_stand_supp",
+           "who_walk_supp", "who_stand_nosupp", "who_walk_nosup" )
 
 #Fit models
 H1_who_models <- NULL
 for(i in Xvars){
   for(j in Yvars){
-    res_unadj <- fit_HR_GAM(d=d, X=i, Y=j, W=NULL)
+    res_unadj <- fit_HR_GAM(d=d, X=i, Y=j, age = "agedays_motor", 
+                            W=NULL, 
+                            forcedW = NULL,
+                            V = NULL,
+                            id = "childid",
+                            pval = 0.2,
+                            print = TRUE)
     res <- data.frame(X=i, Y=j, fit=I(list(res_unadj$fit)), dat=I(list(res_unadj$dat)))
     H1_who_models <- bind_rows(H1_who_models, res)
   }
 }
+
 #Get primary contrasts
 H1_who_res <- NULL
 for(i in 1:nrow(H1_who_models)){
@@ -64,51 +71,15 @@ Wvars[!(Wvars %in% colnames(d))]
 #Add in time varying covariates:
 
 Wvars2_anthro<-c("ageday_at2", "month_at2")
-Wvars3_anthro<-c("ageday_at3", "month_at3", "diar7d_t3", "cesd_sum_ee_t3", "pss_sum_mom_t3", "life_viol_any_t3")  
 
 Wvars2_F2<-c("ageday_ut2", "month_ut2") 
-Wvars3_vital<-c("laz_t2", "waz_t2", "ageday_t3_vital", "month_vt3", "cesd_sum_ee_t3", "pss_sum_mom_t3", "diar7d_t3", "life_viol_any_t3") 
-Wvars3_salimetrics<-c("laz_t2", "waz_t2", "ageday_t3_salimetrics", "month_lt3", "cesd_sum_ee_t3", "pss_sum_mom_t3", "diar7d_t3", "life_viol_any_t3") 
-Wvars3_oragene<-c("laz_t2", "waz_t2", "ageday_t3_oragene", "month_ot3", "cesd_sum_ee_t3", "pss_sum_mom_t3", "diar7d_t3", "life_viol_any_t3") 
 
 
-
-W2_F2.W2_anthro <- c(Wvars, Wvars2_F2, Wvars2_anthro) %>% unique(.)
-W2_F2.W3_anthro <- c(Wvars, Wvars2_F2, Wvars3_anthro, 
-                     "laz_t2", "waz_t2") %>% unique(.)
-W2_F2.W23_anthro <- c(Wvars, Wvars2_F2, Wvars2_anthro, Wvars3_anthro)
-
-
-W3_vital.W3_anthro <- c(Wvars, Wvars3_vital, Wvars3_anthro) %>% unique(.)
-W3_salimetrics.W3_anthro <- c(Wvars, Wvars3_salimetrics, Wvars3_anthro) %>% unique(.)
-W3_oragene.W3_anthro <- c(Wvars, Wvars3_oragene, Wvars3_anthro) %>% unique(.)
-
-
-#pick covariates function
-
-pick_covariates <- function(i, j){
-  # i is exposure as string
-  # j is outcome as string
-  # choose correct/build correct adjustment set based on exposure and outcome
-  if(grepl("t2_f2", i)){
-    if(grepl("_t2_t3", j)){Wset = W2_F2.W23_anthro}
-    else if(grepl("_t2", j)){Wset = W2_F2.W2_anthro}
-    else if(grepl("_who", j)){Wset = W2_F2.W2_anthro}
-    else if(grepl("_t3", j)){Wset = W2_F2.W3_anthro}
-    else if(grepl("_t3", j)){Wset = W2_F2.W3_anthro}}
-  else if(grepl("slope", i)){Wset = c(W3_salimetrics.W3_anthro, "t3_col_time_z01_cont")}
-  else if(grepl("residual", i)){Wset = W3_salimetrics.W3_anthro}
-  else if(i %in% c("t3_map", "t3_hr_mean")){Wset = W3_vital.W3_anthro}
-  else{Wset = W3_oragene.W3_anthro}
-  
-  if(j=="hcz_t3"){Wset=c(Wset, "hcz_t2")}
-  return(Wset)
-}
 
 # --------------------------------------------------------------------------
 #### Hypothesis 1a ####
 
-H1_W <- c(Wvars)
+H1a_W <- c(Wvars, Wvars2_anthro, Wvars2_F2)
 
 ##########################
 
@@ -116,8 +87,8 @@ H1_W <- c(Wvars)
 
 Xvars <- c("t2_f2_8ip", "t2_f2_23d", "t2_f2_VI", "t2_f2_12i", "t2_f2_iso.pca")              
 
-Yvars <- c('ageday_st1', 'agedays_motor', 
-               'month_st1', 'month_mm')
+Yvars <- c("who_sit_nosupp", "who_crawl_nosupp", "who_stand_supp",
+           "who_walk_supp", "who_stand_nosupp", "who_walk_nosup" )
 
 #Fit models
 H1a_who_adj_models <- NULL
@@ -125,13 +96,17 @@ for(i in Xvars){
   for(j in Yvars){
     print(i)
     print(j)
-    Wset<-pick_covariates(i, j)
-    res_adj <- fit_HR_GAM(d=d, X=i, Y=j, age = "agedays_motor", W=Wset)
+    res_adj <- fit_HR_GAM(d=d, X=i, Y=j, age = "agedays_motor", 
+                          W=H1a_W, 
+                          forcedW = NULL,
+                          V = NULL,
+                          id = "childid",
+                          pval = 0.2,
+                          print = TRUE)
     res <- data.frame(X=i, Y=j, fit=I(list(res_adj$fit)), dat=I(list(res_adj$dat)))
     H1a_who_adj_models <- bind_rows(H1a_who_adj_models, res)
   }
 }
-
 
 
 #Get primary contrasts
