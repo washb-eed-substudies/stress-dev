5eddc9c66c2b92b766aaed97c77af04c4c433ba6
Zach <zbutzin@gmail.com>
2021-09-24 10:36:37 -0700

    Updated table code, adding new Table 3 (splitting H1a and H1b)

--- tables/stress-dev-tables.R
+++ tables/stress-dev-tables.R
@@ -119,11 +119,28 @@ names(tbl1)<- c("","","","n (%) or median (IQR)")
 #FOR GATES MILESTONES
 #### Table 2 ####
 
-#previously, there were two t2_f2_8ip, i edited it so tghat there is now "t2_f2_8ip", "t2_f2_23d" based on my ipv, stress, dep - child stress tables 
+#previously, there were two t2_f2_8ip, i edited it so that there is now "t2_f2_8ip", "t2_f2_23d" based on my ipv, stress, dep - child stress tables 
 exposure <- c("t2_f2_8ip", "t2_f2_23d","t2_f2_VI", "t2_f2_12i", "t2_f2_iso.pca")
 outcome <- c("sum_who", "z_cdi_say_t2","z_cdi_und_t2","z_comm_easq", "z_motor_easq","z_personal_easq","z_combined_easq", 
              "z_cdi_say_t3","z_cdi_und_t3")
-expo_var <- c("IPF(2a)-III", "2,3-dinor-iPF(2a)-III", "iPF(2a)-VI", "8,12-iso-iPF(2a)-VI", "Combined urinary oxidative stress biomarkers")
+expo_var <- c("IPF(2a)-III (ng/mg creatinine)", "2,3-dinor-iPF(2a)-III (ng/mg creatinine)", "iPF(2a)-VI (ng/mg creatinine)", "8,12-iso-iPF(2a)-VI (ng/mg creatinine)", "Combined urinary oxidative stress biomarkers (ng/mg creatinine)")
+out_var <- c("Sum of 2nd, 4th, 5th, and 6th WHO motor milestones", "CDI expressive language Z-score Year 1","CDI comprehension Z-score Year 1",
+             "EASQ Communication Score", "EASQ Gross Motor Score", "EASQ Personal Social Score", "Combined EASQ",
+             "CDI expressive language Z-score Year 2","CDI comprehension Z-score Year 2")
+
+tbl2 <- growth_tbl("Urinary isoprostanes at Year 1 and child development at Years 1 and 2", expo_var, out_var, exposure, outcome, H1a, H1a_adj)
+tbl2flex <- growth_tbl_flex("Urinary isoprostanes at Year 1 and child development at Years 1 and 2", expo_var, out_var, exposure, outcome, H1a, H1a_adj)
+
+######
+#New table 3: Urinary isoprostanes at Year 1 and child development at Year 2
+
+#### Table 2 ####
+
+#previously, there were two t2_f2_8ip, i edited it so that there is now "t2_f2_8ip", "t2_f2_23d" based on my ipv, stress, dep - child stress tables 
+exposure <- c("t2_f2_8ip", "t2_f2_23d","t2_f2_VI", "t2_f2_12i", "t2_f2_iso.pca")
+outcome <- c("sum_who", "z_cdi_say_t2","z_cdi_und_t2","z_comm_easq", "z_motor_easq","z_personal_easq","z_combined_easq", 
+             "z_cdi_say_t3","z_cdi_und_t3")
+expo_var <- c("IPF(2a)-III (ng/mg creatinine)", "2,3-dinor-iPF(2a)-III (ng/mg creatinine)", "iPF(2a)-VI (ng/mg creatinine)", "8,12-iso-iPF(2a)-VI (ng/mg creatinine)", "Combined urinary oxidative stress biomarkers (ng/mg creatinine)")
 out_var <- c("Sum of 2nd, 4th, 5th, and 6th WHO motor milestones", "CDI expressive language Z-score Year 1","CDI comprehension Z-score Year 1",
              "EASQ Communication Score", "EASQ Gross Motor Score", "EASQ Personal Social Score", "Combined EASQ",
              "CDI expressive language Z-score Year 2","CDI comprehension Z-score Year 2")
@@ -132,7 +149,7 @@ tbl2 <- growth_tbl("Urinary isoprostanes at Year 1 and child development at Year
 tbl2flex <- growth_tbl_flex("Urinary isoprostanes at Year 1 and child development at Years 1 and 2", expo_var, out_var, exposure, outcome, H1a, H1a_adj)
 
 
-#### Table 3 ####
+#### Table 4 ####
 #Hypothesis 2#
 exposure <- c("t3_cort_slope", "t3_cort_z01", "t3_cort_z03", "t3_saa_slope", "t3_saa_z01", "t3_saa_z02" )
 outcome <- c("z_comm_easq_t3", "z_motor_easq_t3", "z_personal_easq_t3", "z_combined_easq_t3", 
@@ -144,11 +161,11 @@ out_var <- c("EASQ Communication Score", "EASQ Gross Motor Score", "EASQ Persona
              "CDI expressive language Z-score","CDI comprehension Z-score")
 
 
-tbl3 <- growth_tbl("Salivary Stress Biomarkers and Child Development at Year 2", expo_var, out_var, exposure, outcome, H2, H2_adj)
-tbl3flex <- growth_tbl_flex("Salivary Stress Biomarkers and Development at Year 2", expo_var, out_var, exposure, outcome, H2, H2_adj)
+tbl4 <- growth_tbl("Salivary Stress Biomarkers and Child Development at Year 2", expo_var, out_var, exposure, outcome, H2, H2_adj)
+tbl4flex <- growth_tbl_flex("Salivary Stress Biomarkers and Development at Year 2", expo_var, out_var, exposure, outcome, H2, H2_adj)
 
 
-#### Table 4 ####
+#### Table 5 ####
 #Hypothesis 3#
 exposure <- c("t3_map", "t3_hr_mean")  
 outcome <- c("z_comm_easq_t3", "z_motor_easq_t3", "z_personal_easq_t3", "z_combined_easq_t3", 
@@ -157,10 +174,10 @@ expo_var <- c("Mean arterial pressure", "Mean Resting Heart Rate")
 out_var <- c("EASQ Communication Score", "EASQ Gross Motor Score", "EASQ Personal Social Score", "Combined EASQ",
              "CDI expressive language Z-score","CDI comprehension Z-score")
 
-tbl4 <- growth_tbl("Mean arterial pressure and heart rate at year 2 v. development at year 2", expo_var, out_var, exposure, outcome, H3, H3_adj)
-tbl4flex <- growth_tbl_flex("Mean arterial pressure and heart rate at year 2 v. development at year 2", expo_var, out_var, exposure, outcome, H3, H3_adj)
+tbl5 <- growth_tbl("Mean arterial pressure and heart rate at year 2 v. development at year 2", expo_var, out_var, exposure, outcome, H3, H3_adj)
+tbl5flex <- growth_tbl_flex("Mean arterial pressure and heart rate at year 2 v. development at year 2", expo_var, out_var, exposure, outcome, H3, H3_adj)
 
-#### Table 5 ####
+#### Table 6 ####
 #Hypothesis 4#
 
 exposure <- c("t3_gcr_mean", "t3_gcr_cpg12")   
@@ -170,10 +187,10 @@ expo_var <- c("Mean Overall Percentage Glucocorticoid Receptor Methylation", "Pe
 out_var <- c("EASQ Communication Score", "EASQ Gross Motor Score", "EASQ Personal Social Score", "Combined EASQ",
              "CDI expressive language Z-score","CDI comprehension Z-score")
 
-tbl5 <- growth_tbl("Glucocortoic receptor methylation and child development at Year 2", expo_var, out_var, exposure, outcome, H4, H4_adj)
-tbl5flex <- growth_tbl_flex("Glucocortoic receptor methylation and child development at Year 2", expo_var, out_var, exposure, outcome, H4, H4_adj)
+tbl6 <- growth_tbl("Glucocortoic receptor methylation and child development at Year 2", expo_var, out_var, exposure, outcome, H4, H4_adj)
+tbl6flex <- growth_tbl_flex("Glucocortoic receptor methylation and child development at Year 2", expo_var, out_var, exposure, outcome, H4, H4_adj)
 
-#Table 6. Hypothesis 1: WHO Hazard Ratios
+#Table 7. WHO Hazard Ratios
 
 exposure <- c("t2_f2_8ip", "t2_f2_23d", "t2_f2_VI", "t2_f2_12i", "iso.pca")    
 
@@ -184,8 +201,8 @@ expo_var <- c("IPF(2a)-III", "2,3-dinor-iPF(2a)-III", "iPF(2a)-VI", "8,12-iso-iP
 out_var <- c("Time to sitting unsupported", "Time to crawling","Time to standing with support",
              "Time to walking with support", "Time to standing unsupported", "Time to walking unsupported")
 
-tbl6 <- hr_tbl("Urinary isoprostanes and time to WHO motor milestone at Year 1", expo_var, out_var, exposure, outcome, H1a_who, H1a_who_adj)
-tbl6flex <- hr_tbl_flex("Urinary isoprostanes and time to WHO motor milestone at Year 1", expo_var, out_var, exposure, outcome, H1a_who, H1a_who_adj)
+tbl7 <- hr_tbl("Urinary isoprostanes and time to WHO motor milestone at Year 1", expo_var, out_var, exposure, outcome, H1a_who, H1a_who_adj)
+tbl7flex <- hr_tbl_flex("Urinary isoprostanes and time to WHO motor milestone at Year 1", expo_var, out_var, exposure, outcome, H1a_who, H1a_who_adj)
 
 #### SAVE TABLES ####
 write.csv(tbl1, here('tables/stress-dev-table1.csv'))
@@ -194,6 +211,7 @@ write.csv(tbl3, here('tables/stress-dev-table3.csv'))
 write.csv(tbl4, here('tables/stress-dev-table4.csv'))
 write.csv(tbl5, here('tables/stress-dev-table5.csv'))
 write.csv(tbl6, here('tables/stress-dev-table6.csv'))
+write.csv(tbl7, here('tables/stress-dev-table6.csv'))
 
-save_as_docx("Table 1" = tbl1flex, "Table 2" = tbl2flex, "Table 3" = tbl3flex, "Table 4" = tbl4flex, "Table 5" = tbl5flex, "Table 6" = tbl6flex,  path='tables/stress-dev-tables.docx')
+save_as_docx("Table 1" = tbl1flex, "Table 2" = tbl2flex, "Table 3" = tbl3flex, "Table 4" = tbl4flex, "Table 5" = tbl5flex, "Table 6" = tbl6flex, "Table 7" = tbl7flex,  path='tables/stress-dev-tables.docx')
 
